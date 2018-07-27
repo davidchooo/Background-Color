731662fe424f3b145b718f8321c32af4f234f11c
David Choo <davidchoo221@gmail.com>
2018-07-26 22:46:42 -0400

    stack buttons in options page

--- options.js
+++ options.js
@@ -1,4 +1,6 @@
 let page = document.getElementById('buttonDiv');
+page.setAttribute('style', 'white-space: pre-line;');
+
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 function constructOptions(kButtonColors) {
     for (let item of kButtonColors) {
@@ -8,6 +10,7 @@ function constructOptions(kButtonColors) {
         button.style.backgroundColor = item;
         page.appendChild(button);
         page.appendChild(input);
+        page.appendChild(document.createTextNode("\n"));
     }
 }
 constructOptions(kButtonColors);
